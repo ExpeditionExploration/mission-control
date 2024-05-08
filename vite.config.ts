afcbd4346cedea335fbcbf5aa8407b01cb89a99a
Kyle <kyle.ueckermann@gmail.com>
2024-05-09 00:42:33 +0300

    Working

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,31 @@
 import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
-// import react from '@vitejs/plugin-react'
-import babel from 'vite-plugin-babel';
+import react from '@vitejs/plugin-react-swc'
+import path from 'node:path'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { fileURLToPath, URL } from "node:url";
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [babel({
-        babelConfig: {
-            babelrc: false,
-            configFile: false,
-            presets: ["@babel/preset-react", "@babel/preset-typescript"],
-            // plugins: ['babel-plugin-transform-typescript-metadata']
-            // plugins: [
-            //     [
-            //         "@babel/plugin-proposal-decorators",
-            //         { legacy: true },
-            //     ],
-            // ],
-        },
+    build: {
+        rollupOptions: {
+            input: {
+                client: path.join(__dirname, './index.html'),
+                server: path.join(__dirname, './src/server/server.ts'),
+            },
+            output: {
+                entryFileNames: '[name].js',
+            }
+        }
+    },
+    // Set the output directory to the dist folder
+
+    plugins: [react({
+        tsDecorators: true,
     })],
+    resolve: {
+        alias: [
+            { find: '@', replacement: fileURLToPath(new URL('./src/index.ts', import.meta.url)) },
+        ]
+    }
 })
