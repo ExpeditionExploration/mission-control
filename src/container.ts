19b26375e2815c37e9a2ae4bb40676c3a1c764ca
Kyle <kyle.ueckermann@gmail.com>
2024-05-19 22:14:45 +0300

    WIP

--- src/container.ts
+++ src/container.ts
@@ -4,19 +4,15 @@ import { ModuleLoader } from 'src/module-loader';
 import { Module } from 'src/module';
 import { Broadcaster } from 'src/broadcaster';
 import { Config } from './config';
+import * as awilix from 'awilix';
 
-const container = new Container();
-container.bind<string>('namspace').toConstantValue('application');
-container.bind<Config>(Config).to(Config).inSingletonScope();
-container.bind<Broadcaster>(Broadcaster).to(Broadcaster).inSingletonScope();
-container.bind<ModuleLoader>(ModuleLoader).to(ModuleLoader).inSingletonScope();
-// container.registerSingleton(Config, Config);
-// container.registerSingleton(Broadcaster, Broadcaster);
-// container.register(Module, {
-//     useClass: Module
-// }, {
-//     lifecycle: Lifecycle.ContainerScoped
-// });
-// container.registerSingleton(ModuleLoader, ModuleLoader);
+const container = awilix.createContainer({
+    injectionMode: awilix.InjectionMode.PROXY,
+    strict: true,
+});
+// container.bind<string>('namspace').toConstantValue('application');
+// container.bind<Config>(Config).to(Config).inSingletonScope();
+// container.bind<Broadcaster>(Broadcaster).to(Broadcaster).inSingletonScope();
+// container.bind<ModuleLoader>(ModuleLoader).to(ModuleLoader).inSingletonScope();
 
 export { container };
