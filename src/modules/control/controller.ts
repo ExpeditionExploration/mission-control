a0574f661b71c457b0e483f96bcef1705dacdf68
Kyle <kyle.ueckermann@gmail.com>
2024-07-21 21:48:05 +0300

    WIP

--- src/modules/control/controller.ts
+++ src/modules/control/controller.ts
@@ -7,4 +7,31 @@ export class ControlModuleController extends Module {
             console.log('Aileron', data);
         });
     }
+
+    /**
+     * Maps the axis to the aileron left and right rotation target
+     */
+    mapAxisToAileron(axis: Axis) {
+        /**
+         * x = 0 y = 1 -> down -> -90 degrees (left), -90 degrees (right)
+         * x = 0, y = -1 -> up -> 90 degrees (left), 90 degrees (right)
+         * x = 1, y = 0 -> right -> -90 degrees (left), 90 degrees (right)
+         * x = -1, y = 0 -> left -> 90 degrees (left), -90 degrees (right)
+         */
+
+        const output = {
+            left: 0,
+            right: 0,
+        }
+        const { x, y } = axis;
+
+        output.left = this.mapValue(y, -1, 1, 90, -90);
+        output.right = this.mapValue(y, -1, 1, -90, 90);
+
+        return output;
+    }
+
+    mapValue(value: number, inMin: number, inMax: number, outMin: number, outMax: number) {
+        return ((value - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+    }
 }
