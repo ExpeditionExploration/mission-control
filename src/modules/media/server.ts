3dc3836b5e0824ba36f25a5dfa8c36fefd7f7952
Kyle Ueckermann <kyle.ueckermann@gmail.com>
2024-09-16 16:51:42 +0800

    Added media stream

--- src/modules/media/server.ts
+++ src/modules/media/server.ts
@@ -1,105 +1,41 @@
 import { Module } from 'src/module';
 import { ChildProcess, spawn } from 'child_process';
 import { WebSocketServer } from 'ws';
-import { Writable } from 'stream';
+import { Transform, Writable } from 'stream';
 import { ModuleDependencies } from 'src/module-loader';
+
+import * as p2j from 'pipe2jpeg';
+const Pipe2Jpeg: {
+    new (): Transform;
+} = p2j.default;
 
 export class MediaModuleServer extends Module {
     private wss!: WebSocketServer;
     private stream!: ChildProcess;
     private streamPipe: Writable = new Writable();
 
+    private p2j = new Pipe2Jpeg();
+
     createMediaServer(): void {
         this.wss = new WebSocketServer({ port: 16600 });
-        this.streamPipe._write = (chunk, encoding, callback) => {
+        this.stream = spawn(
+            'v4l2-ctl -d0 --set-fmt-video=width=3840,height=2160 --stream-mmap --stream-to -',
+            {
+                shell: true,
+            },
+        );
+        this.p2j.on('data', (data: Buffer) => {
             this.wss.clients.forEach((client) => {
                 if (client.readyState === 1) {
-                    client.send(chunk);
+                    client.send(data, { binary: true });
                 }
             });
-            callback();
-        };
-        // this.streamPipe.on('data', (chunk) => {
-        //     this.wss.clients.forEach((client) => {
-        //         if (client.readyState === 1) {
-        //             client.send(chunk);
-        //         }
-        //     });
-        // });
-
-        // createServer((req, res) => {
-        //     // this.streamFile = fs.createReadStream(STREAM_FILE_PATH);
-        //     console.log('Media server request');
-        //     res.writeHead(200, {
-        //         'Content-Type': 'video/mp4',
-        //     });
-        //     // res.setHeader('Transfer-Encoding', 'chunked');
-        //     // res.setHeader('Connection', 'keep-alive');
-        //     // fifoStream.pipe(res);
-        //     this.streamFile.pipe(res);
-        // }).listen(16600, () => {
-        //     console.log('Media server is running on http://localhost:16600');
-        // });
-    }
-    startMediaStream(): void {
-        console.log('Starting media stream');
-        // try {
-        //     fs.unlinkSync(STREAM_FILE_PATH);
-
-        // } catch (e) { }
-
-        this.stream = spawn(
-            'ffmpeg',
-            [
-                '-loglevel',
-                'error',
-                '-nostats',
-                '-f',
-                'avfoundation',
-                '-r',
-                '30',
-                '-i',
-                '0', // Replace '0' with your webcam device index if different
-                '-c:v',
-                'libx265', // Encode using H.264
-                '-an',
-                '-preset',
-                'ultrafast', // Set encoding speed/quality
-                '-tune',
-                'zerolatency', // Tune for low-latency streaming
-                '-f',
-                'mpegts', // Output format
-                // Output to stdout
-                '-',
-            ],
-            {
-                stdio: ['inherit', 'pipe', 'inherit'],
-            },
-        );
-
-        this.stream.stdout?.pipe(this.streamPipe);
-    }
-
-    async startMjpegStream() {
-        this.stream = spawn(
-            'v4l2-ctl',
-            [
-                '-d0',
-                '--stream-mmap',
-                '--stream-to',
-                // Output to stdout
-                '-',
-            ],
-            {
-                stdio: ['inherit', 'pipe', 'inherit'],
-            },
-        );
+        });
+        this.stream.stdout.pipe(this.p2j);
     }
 
     async onModuleInit() {
         this.logger.info('Starting media server');
-        // this.startMediaStream();
         this.createMediaServer();
-        this.startMjpegStream();
     }
 }
