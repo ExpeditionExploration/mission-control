a8588887a8dd29786178160096b46e50ebaa734b
Mikko Kujala <m.kujala@live.com>
2025-09-11 08:55:17 +0300

    Add battery icon

--- src/modules/battery/components/battery.tsx
+++ src/modules/battery/components/battery.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { ViewProps } from 'src/client/user-interface';
+import { BatteryWarning, BatteryLow, BatteryMedium, BatteryFull } from "lucide-react";
 import { BatteryModuleClient } from "../client";
 
 
@@ -8,15 +9,29 @@ export const Battery: React.FC<ViewProps<BatteryModuleClient>> = ({
 }) => {
 
     const [batteryLevel, setBatteryLevel] = React.useState<number | null>(null);
+    const [batteryIcon, setBatteryIcon] = React.useState<React.ReactNode>(<BatteryFull />);
 
     module.on('status', (status) => {
         setBatteryLevel(status.level);
         module.logger.debug('Battery status', status);
     });
+
+    useEffect(() => {
+        if (batteryLevel < 12) {
+            setBatteryIcon(<BatteryWarning color="red" />);
+        } else if (batteryLevel < 30) {
+            setBatteryIcon(<BatteryLow color="orange" />);
+        } else if (batteryLevel < 70) {
+            setBatteryIcon(<BatteryMedium />);
+        } else {
+            setBatteryIcon(<BatteryFull />);
+        }
+    }, [batteryLevel]);
+
     return (
-        <div>
-            <h3>Battery Level</h3>
-            <p>{batteryLevel ?? 100}%</p>
+        <div className="flex flex-x-space-2 rounded-full justify-center items-center duration-500 transition-colors">
+            {batteryIcon}
+            <h3 className="ml-2">{batteryLevel ?? '?'}</h3>
         </div>
     );
 };
